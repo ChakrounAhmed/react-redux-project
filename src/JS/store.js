67a24b09ccce03e160efd4344765168e43e2920f
Ahmed CHAKROUN <102046411+ChakrounAhmed@users.noreply.github.com>
2023-06-19 17:04:09 +0100

    Update store.js

--- src/JS/store.js
+++ src/JS/store.js
@@ -1,4 +1,4 @@
 import { createStore } from "redux";
 import rootReducers from "./reducers";
 
-export default createStore(rootReducers);
+export default createStore(rootReducers,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
